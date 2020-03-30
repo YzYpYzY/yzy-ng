deeef259d5a7e1b978fd81a5adf80b547dfdfa41
YzYpYzY <culot.jerome@gmail.com>
2020-03-30 20:36:59 +0200

    fix: wrong meta data generated by short import syntax

--- projects/yzy-ng/src/lib/layout/layout.module.ts
+++ projects/yzy-ng/src/lib/layout/layout.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { FilterBarComponent } from './filter-bar/filter-bar.component';
-import { YzYFormsModule } from '../form';
-
-
+import { YzYFormsModule } from '../form/form.module';
 
 @NgModule({
   declarations: [HeaderBarComponent, FilterBarComponent],
