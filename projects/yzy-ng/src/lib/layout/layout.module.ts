990509f01528a08728b469277b0c2aedae878cca
YzYpYzY <culot.jerome@gmail.com>
2020-04-02 19:41:18 +0200

    feat: add some css variable

--- projects/yzy-ng/src/lib/layout/layout.module.ts
+++ projects/yzy-ng/src/lib/layout/layout.module.ts
@@ -2,17 +2,11 @@ import { PipeModule } from './../pipes/pipe.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarComponent } from './bar/bar.component';
-import { YzYFormsModule } from '../form';
-
-
+import { YzYFormsModule } from '../form/form.module';
 
 @NgModule({
-  declarations: [BarComponent],
-  imports: [
-    CommonModule,
-    PipeModule,
-    YzYFormsModule
-  ],
-  exports: [BarComponent]
+    declarations: [BarComponent],
+    imports: [CommonModule, PipeModule, YzYFormsModule],
+    exports: [BarComponent]
 })
-export class LayoutModule { }
+export class LayoutModule {}
