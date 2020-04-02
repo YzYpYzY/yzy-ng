ba3ec7543be467e7ffbe623b84160b8299b2d741
YzYpYzY <culot.jerome@gmail.com>
2020-04-02 19:40:16 +0200

    fix radio field

--- projects/yzy-ng/src/lib/radio/radio.component.ts
+++ projects/yzy-ng/src/lib/radio/radio.component.ts
@@ -6,12 +6,11 @@ import { takeUntil } from 'rxjs/operators';
 import { BaseComponent } from '../base/base.component';
 
 @Component({
-  selector: 'yzy-radio',
-  templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.scss']
+    selector: 'yzy-radio',
+    templateUrl: './radio.component.html',
+    styleUrls: ['./radio.component.scss']
 })
 export class RadioComponent extends BaseComponent implements OnInit {
-
     @Input() fieldModel: FieldModel;
     @Input() form: FormGroup;
 
@@ -27,35 +26,48 @@ export class RadioComponent extends BaseComponent implements OnInit {
 
     constructor() {
         super();
-     }
+    }
 
     ngOnInit(): void {
-        this.selectedValue = this.selectedValue !== undefined ? this.selectedValue
-        : this.fieldModel.value !== undefined ? this.fieldModel.value
-        : 0;
-        this.displayLabel = this.label ? this.label : this.fieldModel && this.fieldModel.label ? this.fieldModel.label : null;
-        const controlName = this.fieldModel && this.fieldModel.name ? this.fieldModel.name : 'default';
-        if(this.form === undefined){
+        this.selectedValue =
+            this.selectedValue !== undefined
+                ? this.selectedValue
+                : this.fieldModel.value !== undefined
+                ? this.fieldModel.value
+                : 0;
+        this.options =
+            this.options !== undefined
+                ? this.options
+                : this.fieldModel.options !== undefined
+                ? this.fieldModel.options
+                : [];
+        this.displayLabel = this.label
+            ? this.label
+            : this.fieldModel && this.fieldModel.label
+            ? this.fieldModel.label
+            : null;
+        const controlName =
+            this.fieldModel && this.fieldModel.name
+                ? this.fieldModel.name
+                : 'default';
+        if (this.form === undefined) {
             this.form = new FormGroup({});
             this.form.addControl(controlName, new FormControl());
         }
         this.control = this.form.controls[controlName] as FormControl;
 
-        if(this.isReadOnly){
+        if (this.isReadOnly) {
             this.control.disable();
         }
         this.control.valueChanges
             .pipe(takeUntil(this.destroy$))
-            .subscribe((value) => {
+            .subscribe(value => {
                 this.selectedValue = value;
                 this.valueChange.emit(value);
             });
     }
 
-    handleSelectOption(value){
-        this.control.setValue(value);
+    selectOption(option: OptionModel): void {
+        this.control.setValue(option.value);
     }
-
-
-
 }
