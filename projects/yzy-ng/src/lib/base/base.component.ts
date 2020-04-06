fe29619851426dcf73b7402c4d4da926bea48e3e
YzYpYzY <culot.jerome@gmail.com>
2020-04-06 22:11:30 +0200

    fix: change BaseComponent destroy$ subject exposition

--- projects/yzy-ng/src/lib/base/base.component.ts
+++ projects/yzy-ng/src/lib/base/base.component.ts
@@ -1,15 +1,14 @@
 import { OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 export class BaseComponent implements OnDestroy {
+    private destroy_sub$: Subject<boolean> = new Subject();
+    destroy$: Observable<boolean> = this.destroy_sub$.asObservable();
 
-    destroy$: Subject<boolean> = new Subject();
-
-    constructor() { }
+    constructor() {}
 
     ngOnDestroy(): void {
-        this.destroy$.next(true);
-        this.destroy$.unsubscribe();
+        this.destroy_sub$.next(true);
+        this.destroy_sub$.unsubscribe();
     }
-
 }
